726d51909251cd6a872a6765a4d32a09d2035f7d
Tamal Chowdhury <tamalanwaronline@gmail.com>
2025-01-13 20:09:16 +0600

    add kinde css and js

--- kindeSrc/environment/pages/(kinde)/(default)/page.tsx
+++ kindeSrc/environment/pages/(kinde)/(default)/page.tsx
@@ -10,18 +10,30 @@ import {
 import React from "react";
 import { renderToString } from "react-dom/server.browser";
 
-export default async function Page({ context, request }) {
+function Layout({ request }) {
   return (
     <html>
       <head>
-        <title>Login | My business</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta robot="noindex, nofollow" />
+        <meta name="csrf-token" content={getKindeCSRF()} />
+        {getKindeRequiredCSS()}
+        {getKindeRequiredJS()}
       </head>
       <body>
-        <div>
-          <h1>Custom Login</h1>
-          {getKindeWidget()}
+        <div id="root" data-roast-root="/admin">
+          <main>
+            <h1>Custom Login</h1>
+            {getKindeWidget()}
+          </main>
         </div>
       </body>
     </html>
   );
+}
+
+export default async function Page(event) {
+  const page = await Layout({ ...event });
+  return renderToString(page);
 }
--- kindeSrc/environment/pages/(kinde)/(index)/page.tsx
+++ kindeSrc/environment/pages/(kinde)/(index)/page.tsx
@@ -10,16 +10,23 @@ import {
 import React from "react";
 import { renderToString } from "react-dom/server.browser";
 
-function Layout({ context, request }) {
+function Layout({ request }) {
   return (
     <html>
       <head>
-        <title>Login | My business</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta robot="noindex, nofollow" />
+        <meta name="csrf-token" content={getKindeCSRF()} />
+        {getKindeRequiredCSS()}
+        {getKindeRequiredJS()}
       </head>
       <body>
-        <div>
-          <h1>Custom Login</h1>
-          {getKindeWidget()}
+        <div id="root" data-roast-root="/admin">
+          <main>
+            <h1>Custom Login</h1>
+            {getKindeWidget()}
+          </main>
         </div>
       </body>
     </html>
